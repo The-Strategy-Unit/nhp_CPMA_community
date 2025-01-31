e79e8a0420a2106fd5cab5b7e366c4a74975c812
anyaferguson <anya.ferguson@nhs.net>
2025-01-31 13:18:14 +0000

    Adding targets

---
+++ _targets.R
@@ -0,0 +1,19 @@
+# Targets for nhp_community_mitigators analysis.
+
+# Load packages required to define the pipeline:
+library(targets)
+# library(tarchetypes) # Load other packages as needed.
+
+# Set target options:
+tar_option_set(
+  packages = c("tibble") 
+)
+
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source()
+# tar_source("other_functions.R") # Source other scripts as needed.
+
+# Replace the target list below with your own:
+list(
+  
+)
