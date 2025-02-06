f16124d39aee7233526e4b52d0f5dba52133fed9
anyaferguson <anya.ferguson@nhs.net>
2025-02-06 10:18:58 +0000

    conneting to databricks

--- _targets.R
+++ _targets.R
@@ -5,34 +5,44 @@ library(targets)
 # library(tarchetypes) # Load other packages as needed.
 
 # Set target options:
-tar_option_set(
-  packages = c("dbplyr", 
-               "dplyr",
-               "janitor", 
-               "readxl", 
-               "sparklyr",
-               "tibble", 
-               "tidyr") 
+tar_option_set(packages = c(
+  "dbplyr",
+  "dplyr",
+  "janitor",
+  "readxl",
+  "sparklyr",
+  "tibble",
+  "tidyr"
+))
+
+# Connection to Databricks
+sc <- sparklyr::spark_connect(
+  master     = Sys.getenv("DATABRICKS_HOST"),
+  cluster_id = Sys.getenv("DATABRICKS_CLUSTER_ID"),
+  token      = Sys.getenv("DATABRICKS_TOKEN"),
+  method     = "databricks_connect",
+  envname    = "r-sparklyr-databricks-15.4" # "r-reticulate"
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
-# tar_source("other_functions.R") # Source other scripts as needed.
 
 # Replace the target list below with your own:
 list(
-  
   # Data wrangling
   tar_target(
     la_population_data,
-    wrangling_la_population_data("Z:/Strategic Analytics/Projects 2025/Describing and Quantifying the NHP mitigators/population_data/myebtablesenglandwales20112023.xlsx"
-)),
-
-tar_target(
-  icb_population_data,
-  wrangling_icb_population_data("Z:/Strategic Analytics/Projects 2025/Describing and Quantifying the NHP mitigators/population_data/sapeicb202420112022.xlsx",
-                                "Z:/Strategic Analytics/Projects 2025/Describing and Quantifying the NHP mitigators/population_data/Integrated_Care_Boards_(December_2024)_Names_and_Codes_in_EN.csv"
-  ))
+    wrangling_la_population_data(
+      "Z:/Strategic Analytics/Projects 2025/Describing and Quantifying the NHP mitigators/population_data/myebtablesenglandwales20112023.xlsx"
+    )
+  ),
   
+  tar_target(
+    icb_population_data,
+    wrangling_icb_population_data(
+      "Z:/Strategic Analytics/Projects 2025/Describing and Quantifying the NHP mitigators/population_data/sapeicb202420112022.xlsx",
+      "Z:/Strategic Analytics/Projects 2025/Describing and Quantifying the NHP mitigators/population_data/Integrated_Care_Boards_(December_2024)_Names_and_Codes_in_EN.csv"
+    )
+  )
   
 )
