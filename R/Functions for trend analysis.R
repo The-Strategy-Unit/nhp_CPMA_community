beb932bbe751a4d1e06368a6cacbe4ec675e39b8
Sarah Lucas <Sarah.Lucas3@nhs.net>
2025-03-05 15:50:56 +0000

    Formating trend charts

--- R/Functions for trend analysis.R
+++ R/Functions for trend analysis.R
@@ -127,37 +127,63 @@ plot_of_standardised_rates_over_time<-function(data){
   
   data1<-data 
   
-  data1|>
-    ggplot()+
-    geom_line(aes(y=value, x=year, group=1), linewidth=1.4)+
+  data1|>    
+    ggplot(aes(y=value, x=year, group=1))+
+    geom_ribbon(aes(ymin = lowercl, ymax = uppercl), fill = "#5881c1" )+
+    geom_line(linewidth=1.4)+
     su_theme()+
     theme(axis.text=element_text(size=10),
           axis.title.y=element_text(size=12))+
     labs(y="Standardised rate per 100,000",
          x=NULL,
-         title=NULL)+ 
-    scale_y_continuous(expand=c(0,0), limits=c(0,max(data1$value)*1.05), labels = label_comma())
+         title=NULL,
+         caption = "Blue ribbon indicates the 95% confidence intervals")+ 
+    scale_y_continuous(expand=c(0,0), limits=c(0,max(data1$uppercl)*1.05), labels = label_comma())
   
   
   }
   
 
-
-
 plotting_icb_over_time<-function(data, axis_title){
+
   
-  data|>
+  ifelse("lowercl" %in% names(data), 
+         data2<-data|>
+           mutate(lower_ci=lowercl)|>
+           mutate(upper_ci=uppercl), 
+         data2<-data|>
+           mutate(lower_ci=NA)|>
+           mutate(upper_ci=NA))
+  
+  ifelse("lowercl" %in% names(data), 
+         footnote<-"Blue ribbon indicates the 95% confidence intervals", 
+         footnote<-"")
+  
+  data2|>
     group_by(icb_2024_name)|>
     highlight_key(~icb_2024_name) |>
-    plot_ly( x = ~year, y = ~activity, type = 'scatter',  mode = 'lines', text=~icb_2024_name,  line = list(color = "#686f73"), width=695, height=332)|>
+    plot_ly( x = ~year, y = ~activity, type = 'scatter',  mode = 'lines', text=~icb_2024_name,  line = list(color = "#686f73"), width=660, height=300)|>
     highlight(~icb_2024_name, on = "plotly_click", off="plotly_doubleclick", dynamic=FALSE)|>
     layout(
-      xaxis = list(title="", showticklabels = TRUE, showline = TRUE, showgrid = F , linewidth=2),
-      yaxis = list(title = axis_title, showline = TRUE, showgrid = F , linewidth=2 ,zeroline = FALSE, tickformat = "digits", anchor="free", shift=100)
-    )
-  
+      xaxis = list(title="", showticklabels = TRUE, showline = TRUE, showgrid = F , linewidth=1.6),
+      yaxis = list(title = axis_title, showline = TRUE, showgrid = F , linewidth=1.6 ,zeroline = FALSE, tickformat = "digits", anchor="free", shift=100),
+      annotations = 
+        list(x = 1, y = -0.16, 
+             text = footnote, 
+             showarrow = F, 
+             xref='paper', 
+             yref='paper'))|>
+    add_ribbons(ymin = ~lower_ci,
+                ymax = ~upper_ci,
+                line=list(color= "#5881c1"),
+                fillcolor =  "#5881c1",
+                opacity = 0.3, 
+                name = '95% ribbon',
+                showlegend = FALSE)
+
   
 }
+
 
 #Plotting change over time
 
@@ -181,15 +207,15 @@ plotting_percentage_change_over_time_by_icb<-function(data, values, eng_average,
     ggplot(aes(x=change, y=fct_reorder(icb_2024_name, change)))+
     geom_bar(stat="identity", fill=ifelse(data1$change<0,  "#70C19A", "#F19388")) +
     geom_vline(xintercept=eng_average, linetype="dashed", 
-               color = "#686f73"   , size=0.4 )+
-    geom_text(aes(x=eng_average, label="England\naverage", y=nrow(data1)+3), colour="#686f73", vjust=1, hjust =-0.09, size=2.7)+
+               color = "black"   , size=0.4 )+
+    geom_text(aes(x=eng_average, label=paste0("England average\n (", eng_average, "%)"), y=nrow(data1)+3), colour="black", vjust=1, hjust =-0.09, size=2.9)+
     su_theme()+
     theme(axis.text.y=element_text(size=8),
           axis.title=element_text(size=10))+
     labs(y=NULL,
          x="Percentage change",
          title=NULL)+
-    geom_text(aes(label=paste0(change, '%')), hjust=ifelse(data1$change<0, 1.1, -0.1), size=2.4)+
+    geom_label(aes(label=paste0(change, '%')), hjust=ifelse(data1$change<0, 1.1, -0.1), size=2.3, label.size = 0)+
     scale_x_continuous(expand=c(0.01,0.01), limits=c((min(data1$change)*min_adjustment),(max(data1$change)*max_adjustment)))
 
   
