46ce4c64d4e0f01c6077d528ca2bfd685efae3dd
anyaferguson <anya.ferguson@nhs.net>
2025-03-28 10:52:19 +0000

    captions for overlaps and trends

--- R/captions.R
+++ R/captions.R
@@ -6,7 +6,7 @@
 #'
 #' @param metric A string.
 #' @param activity_type Either `"admissions"` or `"beddays"`.
-#' @param treatment_type  Either `"emergency"` or `"elective"`.
+#' @param treatment_type  For example `"emergency"` or `"elective"`.
 #'
 #' @return A string.
 format_denominator_for_caption <- function(metric, activity_type, treatment_type) {
@@ -41,16 +41,22 @@ format_group_name_for_caption <- function(group) {
 
 #' Formats the metric shorthand into a metric that can be used in the caption.
 #'
-#' @param metric Either `"number"`, `"perc"`, `"rate"` or` "SR"`.
+#' @param metric Either `"number"`, `"perc"`, `"rate"`, `"prop"` or` "SR"`.
+#' @param treatment_type For example `"emergency"` or `"elective"`.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
 #'
 #' @return A string.
-format_metric_for_caption <- function(metric) {
+format_metric_for_caption <- function(metric, 
+                                      activity_type = NULL, 
+                                      treatment_type = NULL) {
   renamed <- if (metric == "perc") {
     "Percentage"
   } else if(metric == "SR") {
     "Age and sex standardised rates per 100,000 population"
   } else if (metric == "rate") {
-    "Rates per 100,000 population"
+    "Crude rate per 100,000 population"
+  } else if (metric == "prop"){
+    glue::glue("Proportion of {treatment_type} {activity_type}")
   } else { 
     stringr::str_to_title(metric)
   }
@@ -73,7 +79,7 @@ format_treatment_for_caption <- function(treatment_type) {
 #' @param cohort A string for the mitigator cohort.
 #' @param metric Either `"number"`, `"perc"` or` "SR"`.
 #' @param activity_type Either `"admissions"` or `"beddays"`.
-#' @param treatment_type  Either `"emergency"` or `"elective"`.
+#' @param treatment_type  For example `"emergency"` or `"elective"`.
 #'
 #' @return A string.
 get_caption_by_geography <- function(cohort, metric, activity_type, treatment_type = NULL) {
@@ -103,7 +109,7 @@ get_caption_by_geography <- function(cohort, metric, activity_type, treatment_ty
 #'
 #' @param cohort A string for the mitigator cohort.
 #' @param activity_type Either `"admissions"` or `"beddays"`.
-#' @param treatment_type Either `"emergency"` or `"elective"`.
+#' @param treatment_type For example `"emergency"` or `"elective"`.
 #' @param geography Either `"icb"` or `"la"`.
 #'
 #' @return A string.
@@ -114,11 +120,7 @@ get_caption_by_geography_table <- function(cohort,
   
   treatment_type <- format_treatment_for_caption(treatment_type)
   
-  geography <- if(geography == "icb") {
-    "Integrated Care Board"
-  } else {
-    "Local Authority"
-  }
+  geography <- format_geography_for_caption(geography)
   
   if(cohort == "All Mitigation") {
     cohort <- ""
@@ -163,7 +165,7 @@ get_caption_by_group <- function(metric, cohort, activity_type, group, treatment
 #' Get caption for the overview table by mitigator.
 #'
 #' @param cohort A string for the mitigator cohort.
-#' @param treatment_type Either `"emergency"` or `"elective"`.
+#' @param treatment_type For example `"emergency"` or `"elective"`.
 #'
 #' @return A string.
 get_caption_overview <- function(cohort, treatment_type) {
@@ -187,3 +189,127 @@ get_caption_top_ten_specialties <- function(cohort, activity_type) {
   return(caption)
 }
 
+
+
+#' Get caption for the number of cohorts charts.
+#'
+#' @param cohort A string for the mitigator cohort.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
+#'
+#' @return A string.
+get_caption_number_of_cohorts <- function(cohort, activity_type){
+  caption <- glue::glue("The number and percentage of mitigator cohorts that the {cohort} {activity_type} are a part of (2023/24).")
+  
+  return(caption)
+}
+
+#' Get caption for the summary of overlaps charts.
+#'
+#' @param cohort A string for the mitigator cohort.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
+#'
+#' @return A string.
+get_caption_summary_of_overlaps <- function(cohort, activity_type){
+  caption <- glue::glue("The number and proportion of {cohort} {activity_type} that are included in each of the other cohorts (2023/24).")
+  
+  return(caption)
+}
+
+#' Get caption for the upset plots.
+#'
+#' @param cohort A string for the mitigator cohort.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
+#'
+#' @return A string.
+get_caption_upset_plots <- function(cohort, activity_type){
+  caption <- glue::glue("The 15 most common mitigator cohort overlaps for {cohort} {activity_type} (2023/24).")
+  
+  return(caption)
+}
+
+#' Get caption for the trends plots.
+#'
+#' @param cohort A string for the mitigator cohort.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
+#' @param metric Either `"number"`, `"perc"` or` "SR"`.
+#' @param geography Either `"England"`, `"icb"` or `"la"`.
+#'
+#' @return A string.
+get_caption_for_trends <- function(cohort, metric, activity_type, geography) {
+  
+  metric <- format_metric_for_caption(metric) |>
+    stringr::str_to_lower()
+  
+  geography <- if(geography == "England") {
+    "in England"
+  } else {
+    glue::glue("by {format_geography_for_caption(geography)}")
+    }
+  
+  caption <- glue::glue("The {metric} of {activity_type} for the {cohort} cohort {geography} over the last 9 years.")
+  
+  return(caption)
+}
+
+#' Formats the geography so can be used in a caption.
+#'
+#' @param geography Either `"icb"`,`"la"` or other.
+#'
+#' @return A string.
+format_geography_for_caption <- function(geography) {
+  
+  caption <- if(geography == "icb") {
+    "Integrated Care Board"
+  } else if (geography == "la") {
+    "Local Authority"
+  } else {
+    geography
+  }
+  
+  return(caption)
+}
+
+#' Get caption for trends percentage change plots.
+#'
+#' @param cohort A string for the mitigator cohort.
+#' @param metric Either `"number"`, `"prop"` or` "SR"`.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
+#'
+#' @return A string.
+get_caption_for_trends_percentage_change <- function(cohort, 
+                                                     metric, 
+                                                     activity_type, 
+                                                     treatment_type = NULL) {
+  metric <- format_metric_for_caption(metric, activity_type, treatment_type) |>
+    stringr::str_to_lower()
+  
+  caption <- glue::glue("The percentage change in the {metric} of {cohort} {activity_type} by ICB between 2018/19 and 2023/24.")
+  
+  return(caption)
+}
+
+#' Get caption for the total activity vs percentage change plots.
+#'
+#' @param cohort A string for the mitigator cohort.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
+#'
+#' @return A string.
+get_caption_for_activity_vs_perc_change <- function(cohort,
+                                                    activity_type) {
+  caption <- glue::glue("The standardised rate of {cohort} {activity_type} (in 2018/19) versus the percentage change in {activity_type} between 2018/19 and 2023/24 for ICBs.")
+  
+  return(caption)
+}
+
+#' Get caption for the local authority trends tables.
+#'
+#' @param cohort A string for the mitigator cohort.
+#' @param activity_type Either `"admissions"` or `"beddays"`.
+#'
+#' @return A string.
+get_caption_for_la_trends <- function(cohort,
+                                      activity_type) {
+  caption <- glue::glue("Table of the standardised rate of {cohort} {activity_type} by Local Authority over the last 5 years, including the percentage change between 2018/19 and 2023/24.")
+  
+  return(caption)
+}
