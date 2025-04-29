a064747ceb1df3676d44bbcdecb5631bb0d7ffa5
anyaferguson <anya.ferguson@nhs.net>
2025-04-29 01:39:31 +0100

    reducing calculations of max values

--- R/Functions for trend analysis.R
+++ R/Functions for trend analysis.R
@@ -188,22 +188,23 @@ data_number_percentage_over_time_icb<-function(data1, data2, mitigator, treatmen
 plot_of_number_over_time<-function(data,  activity_type){
   
   data2<-data|>
-    group_by(year)|>
-    summarise(activity=sum({{activity_type}}))
+    summarise(activity=sum({{activity_type}}), .by = year)
+  
+  max_number <- max(data2$activity)
   
   data2|>
     ggplot()+
     geom_line(aes(y=activity, x=year, group=1), linewidth=1.4)+
     geom_rect(aes(NULL,NULL,xmin="2019/20",xmax="2021/22"),
-              ymin=0,ymax=max(data2$activity)*1.1, fill="#686f73", size=0.5, alpha=0.01)+
-    annotate("text", x ="2020/21", y = max(data2$activity)*1.08, label = "COVID-19 pandemic", size=2.7)+
+              ymin=0,ymax=max_number*1.1, fill="#686f73", size=0.5, alpha=0.01)+
+    annotate("text", x ="2020/21", y = max_number*1.08, label = "COVID-19 pandemic", size=2.7)+
     su_theme()+
     theme(axis.text=element_text(size=10),
           axis.title.y=element_text(size=12))+
     labs(y="Number",
          x=NULL,
          title=NULL)+ 
-    scale_y_continuous(expand=c(0,0), limits=c(0,max(data2$activity)*1.1), labels = label_comma())
+    scale_y_continuous(expand=c(0,0), limits=c(0,max_number*1.1), labels = label_comma())
   
   
 }
@@ -213,24 +214,26 @@ plot_of_number_over_time<-function(data,  activity_type){
 plot_of_percentage_over_time<-function(data, activity_type, denominator){
   
 data1<-data|>
-  group_by(year)|>
   summarise(number=sum({{activity_type}}),
-            total_number=sum({{denominator}}))|>
+            total_number=sum({{denominator}}),
+            .by = year)|>
   mutate(percentage=janitor::round_half_up((number/total_number)*100,1))
+
+max_percentage <- max(data1$percentage)
 
 data1|>
     ggplot()+
     geom_line(aes(y=percentage, x=year, group=1), linewidth=1.4)+
   geom_rect(aes(NULL,NULL,xmin="2019/20",xmax="2021/22"),
-            ymin=0,ymax=max(data1$percentage*1.1), fill="#686f73", size=0.5, alpha=0.01)+
-  annotate("text", x ="2020/21", y = max(data1$percentage)*1.08, label = "COVID-19 pandemic", size=2.7)+
+            ymin=0,ymax=max(max_percentage)*1.1, fill="#686f73", size=0.5, alpha=0.01)+
+  annotate("text", x ="2020/21", y = max(max_percentage)*1.08, label = "COVID-19 pandemic", size=2.7)+
     su_theme()+
     theme(axis.text=element_text(size=10),
           axis.title.y=element_text(size=12))+
     labs(y="Percentage",
          x=NULL,
          title=NULL)+ 
-    scale_y_continuous(expand=c(0,0), limits=c(0,max(data1$percentage)*1.1), labels = label_comma())
+    scale_y_continuous(expand=c(0,0), limits=c(0,max(max_percentage)*1.1), labels = label_comma())
   
   
   
@@ -239,15 +242,15 @@ data1|>
 
 plot_of_standardised_rates_over_time<-function(data){
   
-  data1<-data 
+  max_value <- max(data$value)
   
-  data1|>    
+  data|>    
     ggplot(aes(y=value, x=year, group=1))+
     geom_ribbon(aes(ymin = lowercl, ymax = uppercl), fill = "#5881c1" )+
     geom_line(linewidth=1.4)+
     geom_rect(aes(NULL,NULL,xmin="2019/20",xmax="2021/22"),
-              ymin=0,ymax=max(data1$value*1.1), fill="#686f73", size=0.5, alpha=0.01)+
-    annotate("text", x ="2020/21", y = max(data1$value)*1.08, label = "COVID-19 pandemic", size=2.7)+
+              ymin=0,ymax=max_value*1.1, fill="#686f73", size=0.5, alpha=0.01)+
+    annotate("text", x ="2020/21", y = max_value*1.08, label = "COVID-19 pandemic", size=2.7)+
     su_theme()+
     theme(axis.text=element_text(size=10),
           axis.title.y=element_text(size=12))+
@@ -255,7 +258,7 @@ plot_of_standardised_rates_over_time<-function(data){
          x=NULL,
          title=NULL,
          caption = "Blue ribbon indicates the 95% confidence intervals")+ 
-    scale_y_continuous(expand=c(0,0), limits=c(0,max(data1$uppercl)*1.1), labels = label_comma())
+    scale_y_continuous(expand=c(0,0), limits=c(0,max(data$uppercl)*1.1), labels = label_comma())
   
   
   }
